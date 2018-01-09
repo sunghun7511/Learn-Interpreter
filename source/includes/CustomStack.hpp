a5f5ff11e2cd86d80962debe2b076f416c28f65f
SHGroup <sunghun7511@naver.com>
2018-01-10 01:40:40 +0900

    base libraries code!

---
+++ source/includes/CustomStack.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stack>
+
+class CustomStack {
+private:
+    std::stack<double> stk;
+public:
+    void push(const double n);
+    const int size();
+    const bool empty();
+    const double pop();
+};
